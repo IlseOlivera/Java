6302aa4745afb2043c41e254ead6acb4deeedee5
IlseOlivera <106711650+IlseOlivera@users.noreply.github.com>
2023-07-24 23:38:44 -0600

    Ejemplo demojpa

---
+++ demojpa/src/main/java/com/jpa/App.java
@@ -0,0 +1,10 @@
+package com.jpa;
+
+import com.jpa.persistencia.PersistenceController;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        PersistenceController controlPersis =new PersistenceController();
+    }
+}
---
+++ demojpa/target/generated-sources/annotations/com/jpa/logica/Alumno_.java
@@ -0,0 +1,17 @@
+package com.jpa.logica;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-07-24T16:09:37")
+@StaticMetamodel(Alumno.class)
+public class Alumno_ { 
+
+    public static volatile SingularAttribute<Alumno, Date> fechaNac;
+    public static volatile SingularAttribute<Alumno, String> apellido;
+    public static volatile SingularAttribute<Alumno, Integer> id;
+    public static volatile SingularAttribute<Alumno, String> nombre;
+
+}
